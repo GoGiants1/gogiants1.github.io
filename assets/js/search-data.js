b2c9c5fcd54093c5ff05067e0a796f33e4777de4
GoGiants1 <69342392+GoGiants1@users.noreply.github.com>
2025-06-15 05:29:29 +0000

    Deploying to gh-pages from @ GoGiants1/gogiants1.github.io@336657ca5c69de0f4e9cef00af26717101f6fc04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-dit-scalable-diffusion-models-with-transformers-dev-in-seoul",
+        },{id: "post-paper-review-presel-pre-instruction-data-selection-for-visual-instruction-tuning",
+        
+          title: "[Paper Review] Presel: Pre-Instruction Data Selection for Visual Instruction Tuning",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/Presel/";
+          
+        },
+      },{id: "post-dit-scalable-diffusion-models-with-transformers-dev-in-seoul",
         
           title: '[DiT] Scalable Diffusion Models with Transformers — Dev In Seoul <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
